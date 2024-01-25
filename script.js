7ce4c0c066b6000f42494a0fe2488918c28250bf
Yeddula Shantan <100433099+shantan125@users.noreply.github.com>
2024-01-25 16:24:10 +0530

    Created the Rock-Paper-Scissor game

---
+++ script.js
@@ -0,0 +1,45 @@
+let choices = document.querySelectorAll(".choice");
+let msg = document.querySelector("#msg");
+let userscore = 0;
+let compscore = 0;
+let userscorepara = document.querySelector("#you-score");
+let compscorepara = document.querySelector("#comp-score");
+let resetbtn = document.querySelector("#reset");
+
+const compturn = () => {
+    const options = ["rock", "paper", "scissor"];
+    const randchoice = Math.floor(Math.random() * 3);
+    return options[randchoice];
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        let yourturn = choice.getAttribute("id");
+        let computerturn = compturn();
+
+        if (yourturn === computerturn) {
+            msg.innerText = "Draw";
+        } else if (
+            (yourturn === 'rock' && computerturn === 'paper') ||
+            (yourturn === 'rock' && computerturn === 'scissor') ||
+            (yourturn === 'paper' && computerturn === 'rock') ||
+            (yourturn === 'scissor' && computerturn === 'paper')
+        ) {
+            msg.innerText = "You won the match";
+            userscore++;
+        } else {
+            msg.innerText = "Computer won the match";
+            compscore++;
+        }
+
+        // Update score display
+        userscorepara.textContent = userscore;
+        compscorepara.textContent = compscore;
+    });
+});
+resetbtn.addEventListener("click",()=>{
+     userscorepara.textContent = 0;
+        compscorepara.textContent = 0;
+        msg.innerText="Play your move";
+});
+
